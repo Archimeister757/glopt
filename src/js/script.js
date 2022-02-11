24e616acd53eb7a0e1bdc248b35070b185c5288b
Artem <mohov.artem95@mail.ru>
2022-02-11 11:55:03 +0300

    cards complete

--- src/js/script.js
+++ src/js/script.js
@@ -10,4 +10,50 @@ window.addEventListener('DOMContentLoaded', () => {
     humburger.classList.toggle("humburger_active");
     menu.classList.toggle("promo__menu_active");
     });
+
+    //Cards
+
+    const btnNext = document.querySelectorAll('[data-next]'),
+          btnBack = document.querySelectorAll('[data-back]'),
+          cardsParent = document.querySelector('.delivery__cards'),
+          cardContent = document.querySelectorAll('.delivery__card-content'),
+          cardList = document.querySelectorAll('.delivery__card-list');
+
+
+    function showCardContent(i = 0) {
+        cardContent[i].classList.add('delivery__card-content_hide');
+        cardList[i].classList.add('delivery__card-list_active');
+    }
+    function hideCardContent(i = 0) {
+        cardContent[i].classList.remove('delivery__card-content_hide');
+        cardList[i].classList.remove('delivery__card-list_active');
+    }
+
+    cardsParent.addEventListener('click', (event) => {
+
+        const target = event.target;
+        
+
+        if (target && target.classList.contains('next')) {
+            
+
+            btnNext.forEach((item, i) => {
+
+                if (target == item) {
+
+                    showCardContent(i);
+                }
+            });
+        } else if (target && target.classList.contains('back')) {
+            console.log('Hello');
+
+            btnBack.forEach((item, i) => {
+
+                if (target == item) {
+
+                    hideCardContent(i);
+                }
+            });
+        }
+    });
 });
