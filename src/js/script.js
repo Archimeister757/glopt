e4d4eb7db02d94266bbd1fd08997a007f33dd882
Artem <mohov.artem95@mail.ru>
2022-04-10 14:06:35 +0300

    slider complete

--- src/js/script.js
+++ src/js/script.js
@@ -47,4 +47,37 @@ window.addEventListener('DOMContentLoaded', () => {
          });
       }
    });
+
+   //Slider
+
+   const slides = document.querySelectorAll('.reviews__block'),
+      arrowBack = document.querySelector('.arrow_back'),
+      arrowNext = document.querySelector('.arrow_next');
+
+   let slideIndex = 1;
+
+   showSlides(slideIndex);
+
+   function showSlides(n) {
+      if (n > slides.length) {
+         slideIndex = 1;
+      }
+      if (n < 1) {
+         slideIndex = slides.length;
+      }
+      slides.forEach((item) => {
+         item.style.display = 'none';
+      });
+      slides[slideIndex - 1].style.display = 'block';
+   }
+
+   function plusSlides(n) {
+      showSlides((slideIndex += n));
+   }
+   arrowNext.addEventListener('click', () => {
+      plusSlides(+1);
+   });
+   arrowBack.addEventListener('click', () => {
+      plusSlides(-1);
+   });
 });
